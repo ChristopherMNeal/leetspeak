3506785924d5589ece6a8116b1d693abcb4dd655
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-06 12:02:56 -0800

    Add and pass tests to support additional letters
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -5,14 +5,26 @@ def leetspeak(input)
   # word = String.new(input)
     letter_array = input.split("")
     new_array = []
+    i = 0
     letter_array.each do |letter|
       if letter == "e"
         new_array.push("3")
       elsif letter == "o"
         new_array.push("0")
+      elsif letter == "I"
+        new_array.push("1")
+      elsif letter == "s"
+        if i == 0
+          new_array.push("s")
+        elsif letter_array[i.to_i - 1] == " "
+          new_array.push("s")
+        else
+          new_array.push("z")
+        end
       else
         new_array.push(letter)
       end
+      i += 1
     end
     new_array.join("")
   end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -8,4 +8,16 @@ describe('#leetspeak') do
   it("returns 0 when o is inputted") do
     expect(leetspeak("eo")).to(eq("30"))
   end
+  it("returns 0 and 3 when o and e are entered with multiple words") do
+    expect(leetspeak("over egg")).to(eq("0v3r 3gg"))
+  end
+  it("returns '1' when 'I' is entered with multiple words") do
+    expect(leetspeak("I Robot")).to(eq("1 R0b0t"))
+  end
+  it("returns 'z' when 's' is entered") do
+    expect(leetspeak("overeasy eggs")).to(eq("0v3r3azy 3ggz"))
+  end
+  it("returns 'z' when 's' is entered") do
+    expect(leetspeak("sassy pants")).to(eq("sazzy pantz"))
+  end
 end
